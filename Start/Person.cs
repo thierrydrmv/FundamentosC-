db1f9c6af78be22e743c518551c9e7f4196e5d95
Thierry Varela <varelathierry@gmail.com>
2024-02-09 08:47:51 -0300

    feat: teoria + start

---
+++ Start/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public string height;
+
+        public Person(string name, int age, string height) {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+        
+    }
+}
---
+++ Start/Program.cs
@@ -0,0 +1,14 @@
+﻿// using responsável pela importação e organização da aplicação
+// Tudo entre as chaves está no escopo do namespace Start
+// Não podem ter duas classes dentro do mesmo namespace com o msm nome
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Carlos = new Person("Carlos", 22, "1.70");
+            Console.WriteLine(Carlos.name);
+        }
+    }
+}
